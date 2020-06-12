2873aec51dcefef2ca0d7e8d90b89cb0599db7eb
Patrick Lee <redundantcomputation@gmail.com>
2020-06-12 11:54:00 -0700

    Setting up HTML for CRUD on station members

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php include "templates/header.php"; ?>
+
+<ul>
+  <li>
+    <a href="createstationmember.php"><strong>Create</strong></a> - Add a new station member
+  </li>
+  <li>
+    <a href="readstationmember.php"><strong>Read</strong></a> - View list of station members
+  </li>
+</ul>
+
+<?php include "templates/footer.php"; ?>
---
+++ public/templates/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <title>Playlist Planet</title>
+
+        <link rel="stylesheet" href="css/style.css" />
+    </head>
+
+    <body>
+        <h1>Playlist Planet</h1>
+    </body>
+</html>
