0a06b577df1329c62554140973be90a81b613021
Patrick Lee <redundantcomputation@gmail.com>
2020-06-12 14:00:10 -0700

    Broken insert operation for station members

--- public/createstationmember.php
+++ public/createstationmember.php
@@ -1,5 +1,44 @@
 <?php include "templates/header.php"; ?>
 
+    <?php
+    if (isset($_POST['submit'])) {
+        require "../config.php";
+
+        try {
+            // If the submission was valid, connect to the database
+            $connection = new PDO($dsn, $username, $password, $options);
+
+            // Store all user info into an array
+            $new_user = array(
+                "first_name"    => $_POST['first_name'],
+                "last_name"     => $_POST['last_name'],
+                "province"      => $_POST['province'],
+                "postalcode"    => $_POST['postalcode'],
+                "pronouns"      => $_POST['pronouns'],
+                "address"       => $_POST['address'],
+                "city"          => $_POST['city'],
+                "email"         => $_POST['email'],
+                "primary_phone" => $_POST['primary_phone'],
+                "alt_phone"     => $_POST['alt_phone'],
+                "interests"     => $_POST['interests'],
+                "skills"        => $_POST['skills']
+              );
+
+            // SQL statement
+            $sql = "INSERT INTO station_members (first_name, last_name, province, postalcode, pronouns, address, city, email, primary_phone, alt_phone, interests, skills)
+                    VALUES (:first_name, :last_name, :province, :postalcode, :pronouns, :address, :city, :email, :primary_phone, :alt_phone, :interests, :skills)";
+            
+            // Prepare and execute SQL statement
+            $statement = $connection->prepare($sql);
+            $statement->execute($new_user);
+
+        } catch(PDOException $error) {
+            echo $sql . "<br>" . $error->getMessage();
+        }
+    }
+    ?>
+
+    <!-- Input form for creating a new station member -->
     <form method="post">
         <label for="first_name">First Name</label>
         <input type="text" name="first_name" id="first_name">
@@ -37,8 +76,8 @@
         <label for="skills">Skills</label>
         <input type="text" name="skills" id="skills">
 
-        <br>
-        <br>
+        </br>
+        </br>
 
         <input type="submit" name="submit" value="Submit">
     </form>
