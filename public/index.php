5bca5d6317a20b0462079f2ea70103e7cd73702b
carolz19 <zhang.carol19@gmail.com>
2020-06-12 17:50:45 -0700

    css gridified

--- public/index.php
+++ public/index.php
@@ -1,15 +1,22 @@
 <?php 
   include "templates/header.php"; 
-  include "public/css/index.css"
 ?>
 
-<ul>
-  <li>
-    <a href="createstationmember.php"><strong>New User</strong></a> - Add a new station member
-  </li>
-  <li>
-    <a href="readstationmember.php"><strong>Search Station Members</strong></a> - Search for station members
-  </li>
-</ul>
+<!-- <style>
+<?php include ''; ?>
+</style> -->
+
+<div class="Index">
+
+  <div class="Header">
+    <h1>Playlist Planet</h1>
+  </div>
+
+  <div class="Menu">
+        <a href="createstationmember.php"><strong>New User</strong></a> - Add a new station member
+        <br/>
+        <a href="readstationmember.php"><strong>Search Station Members</strong></a> - Search for station members
+  </div>
+<div>
 
 <?php include "templates/footer.php"; ?>
--- public/templates/footer.php
+++ public/templates/footer.php
@@ -1,2 +1,6 @@
+<div className="footer">
+    Made by Justin Chan, Patrick Lee, Carol Zhang | 
+    <a href="https://github.com/carolz19/judge"> github </a>
+</div>
 </body>
 </html>
