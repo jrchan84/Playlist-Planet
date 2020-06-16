c7a2d78c253ee34d6ee0a49c036efa56c810e194
carolz19 <zhang.carol19@gmail.com>
2020-06-16 16:21:02 -0700

    styling + adding new aggregation query

--- public/index.php
+++ public/index.php
@@ -14,7 +14,7 @@
 
   <div class="Menu">
         <a href="createstationmember.php"><strong>New User</strong></a> - Add a new station member
-        <br/>
+        <br />
         <a href="readstationmembers.php"><strong>Search Station Members</strong></a> - Search for station members
         <br/>
         <a href="updatestationmembers.php"><strong>Edit Users</strong></a> - Edit station member information
@@ -25,11 +25,12 @@
         <br />
         <a href="projectstationmembers.php"><strong>Find Station Member Information</strong></a> - Find specific types of information about station members
         <br />
-        <a href="no-of-members.php"><strong>Number of Station Members</strong></a> - Get current number of station members
+        <a href="aggregation.php"><strong>Members in Collective</strong></a> - Get current number of station members
         <br />
         <a href="nestedagg.php"><strong>Nested Aggregation Query</strong></a> - Get episodes that are in more than a certain number of shows 
         <br />
         <a href="division.php"><strong>Division Query</strong></a> - See if an episode is in ALL shows
+        <br />
 
   </div>
 
